5a4f67f83a16a30d4a53909f593ab6ea13ea2a4a
logananglin98 <logananglin98@gmail.com>
2024-11-08 23:27:47 -0500

    Renamed foldres. Completed Problem G from 2023. Updated ReadMe.

---
+++ 2023/Problem_G_Triple_Sevens.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    std::ifstream file("Problem_G_input.txt");
+
+    string line;
+    int n;
+    int sevens = 1;
+    int o = 777;
+
+    while(getline(file, line)) {
+        int space = line.find(" ");
+        if (space == -1) {
+            n = stoi(line);
+        } else {
+            sevens = 0;
+            for (int i = 0; i < 1 + (n - 1) * 2; i++) {
+                if (line[i] == '7') {
+                    sevens++;
+                }
+            }
+        }
+        if (sevens < 1) {
+            o = 0;
+            break;
+        }
+    }
+
+    std::ofstream output("Problem_G_output.txt");
+
+    output << o;
+
+    return 0;
+}
--- ReadMe.md
+++ ReadMe.md
@@ -14,3 +14,7 @@ Blueberry Waffle
 I Could Have Won  
 > I believe I have solved this problem correctly, however, I also believe Sample Output 1 on the webpage is incorrect. The input is BBAAABABBAAABB. The suggested output for all values of k is "3 6 7". When running my code that output Sample Output 2 correctly with Sample Input 1, the output for all values was "3 4 6 7". Upon going through Sample Input 1, I discovered that 4 is a value of k that would lead to Alice winning. The final score in a first-to-4 round with the scores would be BBAAABA with Bob having 3 points and Alice having 4.
 
+## 2023 practice problems
+Link to all 2023 problems: https://mcpc23.kattis.com/contests/mcpc23/problems  
+Triple Sevens  
+
